014b7079fe16587659542180db7e5e4e263500e6
MALCOLM STAGG <mstagg3@lsu.edu>
2013-10-07 22:09:37 -0500

    Added code for parsing first-order-logic expressions

---
+++ Main/GeometryTutorLibrary/FirstOrderLogic/FirstOrderLogic/ASTNode.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GeometryTutorLib.FirstOrderLogic
+{
+    class ASTNode
+    {
+        string _name;
+        List<ASTNode> _children;
+        ASTNode _parent;
+
+        public ASTNode(string name, ASTNode parent)
+        {
+            _name = name;
+            _children = new List<ASTNode>();
+            _parent = parent;
+        }
+
+        public ASTNode(string name)
+        {
+            _name = name;
+            _children = new List<ASTNode>();
+            _parent = null;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void AddChild(ASTNode node)
+        {
+            _children.Add(node);
+        }
+
+        public ASTNode GetParent()
+        {
+            return _parent;
+        }
+
+        public void SetParent(ASTNode node)
+        {
+            _parent.DeleteChild(this);
+            _parent = node;
+            _parent.AddChild(this);
+        }
+
+        public int NumChildren()
+        {
+            return _children.Count();
+        }
+
+        public ASTNode GetChild(int num)
+        {
+            return _children[num];
+        }
+
+        public ASTNode GetLastChild()
+        {
+            return _children[_children.Count() - 1];
+        }
+
+        public void DeleteChild(ASTNode node)
+        {
+            _children.Remove(node);
+        }
+
+        private int tabSize = 3;
+        protected void Indent(StringBuilder sb, int tabDepth)
+        {
+            for (int i = 0; i < tabSize * tabDepth; i++)
+            {
+                sb.Append(' ');
+            }
+        }
+
+        public string Unparse()
+        {
+            StringBuilder sb = new StringBuilder();
+            BuildUnparse(sb, 0);
+            return sb.ToString();
+        }
+
+        internal void BuildUnparse(StringBuilder sb, int tabDepth)
+        {
+            Indent(sb, tabDepth);
+            sb.Append(_name);
+            sb.AppendLine();
+            for (int i = 0; i < _children.Count(); i++)
+            {
+                _children[i].BuildUnparse(sb, tabDepth + 1);
+            }
+        }
+    }
+}
---
+++ Main/GeometryTutorLibrary/FirstOrderLogic/FirstOrderLogic/Parser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GeometryTutorLib.FirstOrderLogic
+{
+    class Parser
+    {
+        private struct strOperators
+        {
+            public string name;
+            public int precedence;
+        };
+
+        private static strOperators[] ops =
+            new strOperators[]{
+                new strOperators() { name = "->", precedence = 1 },
+                new strOperators() { name = "=", precedence = 2 },
+                new strOperators() { name = ">", precedence = 2 },
+                new strOperators() { name = "<", precedence = 2 },
+                new strOperators() { name = ">=", precedence = 2 },
+                new strOperators() { name = "<=", precedence = 2 },
+                new strOperators() { name = "+", precedence = 3 },
+                new strOperators() { name = "-", precedence = 3 },
+                new strOperators() { name = "*", precedence = 4 },
+                new strOperators() { name = "/", precedence = 4 },
+                new strOperators() { name = "^", precedence = 5 },
+            };
+
+        private static bool IsOperator(string text)
+        {
+            foreach (strOperators op in ops)
+            {
+                if (op.name == text)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static int OperatorPriority(string text)
+        {
+            foreach (strOperators op in ops)
+            {
+                if (op.name == text)
+                {
+                    return op.precedence;
+                }
+            }
+            return 0;
+        }
+        public static void SplitCorrections(ref string op1, ref string op2)
+        {
+            //some corrections after splitting:
+            if (op1 == "-" && op2 == ">")
+            {
+                op1 = "->";
+                op2 = "";
+            }
+            else if (op1 == "<" && op2 == "=")
+            {
+                op1 = "<=";
+                op2 = "";
+            }
+            else if (op1 == ">" && op2 == "=")
+            {
+                op1 = ">=";
+                op2 = "";
+            }
+
+        }
+        public static ASTNode ParseText(string text)
+        {
+            //remove spaces
+            text = Regex.Replace(text, @" ", @"");
+
+            //split before & after ( , ) - + * / = > <
+            string[] splitstr = Regex.Split(text, @"(?=[(,)-+*/=><])|(?<=[(,)-+*/=><])");
+
+            //string lastElement = "";
+
+            ASTNode root = new ASTNode("");
+            ASTNode parent = root;
+
+            for (int i = 0; i < splitstr.Length; i++)
+            {
+                if (i < splitstr.Length - 1)
+                    SplitCorrections(ref splitstr[i], ref splitstr[i + 1]);
+
+                //main parser:
+                if (splitstr[i] == "(")
+                {
+                    parent = parent.GetLastChild();
+                }
+                else if (splitstr[i] == "," || splitstr[i] == "")
+                {
+                }
+                else if (splitstr[i] == ")")
+                {
+                    parent = parent.GetParent();
+                }
+                else if (IsOperator(splitstr[i]))
+                {
+                    if (OperatorPriority(splitstr[i]) > OperatorPriority(parent.GetName()))
+                    {
+                        ASTNode opnode = new ASTNode(splitstr[i], parent);
+                        ASTNode lastnode = parent.GetLastChild();
+                        lastnode.SetParent(opnode); //change the parent to the operator
+                        parent.AddChild(opnode);
+                        parent = opnode;
+                    }
+                    else
+                    {
+                        ASTNode grandparent = parent.GetParent();
+                        ASTNode opnode = new ASTNode(splitstr[i], grandparent);
+                        parent.SetParent(opnode); //change the parent to the operator
+                        grandparent.AddChild(opnode);
+                        parent = opnode;
+                    }
+                }
+                else
+                {
+                    ASTNode node = new ASTNode(splitstr[i], parent);
+                    parent.AddChild(node);
+                }
+            }
+            return root;
+        }
+    }
+}
