aa624e6051acd4df9ec6a32e44a47bf1426769c4
Brandon Oubre <br.oubre@yahoo.com>
2014-08-03 23:39:23 -0500

    Flood fill algorithm update. Still slow, needs improvement. (Minimize number of number of checks).

--- Main/DynamicGeometryLibrary/UI/RegionShading/ShadedRegion.cs
+++ Main/DynamicGeometryLibrary/UI/RegionShading/ShadedRegion.cs
@@ -92,10 +92,6 @@ namespace DynamicGeometry.UI.RegionShading
         /// <returns>The image, with the ShadedRegion drawn</returns>
         public Image Draw(Drawing drawing, int color1, int color2)
         {
-            //These next two lines are temporary
-            //AtomicRegion sar = Region as AtomicRegion;
-            //if (sar == null) return null;
-
             CoordinateSystem cs = drawing.CoordinateSystem;
             WriteableBitmap bmp = new WriteableBitmap((int)cs.PhysicalSize.X, (int)cs.PhysicalSize.Y);
             Point start = ToPhysical(cs, PointInShape);
@@ -112,6 +108,7 @@ namespace DynamicGeometry.UI.RegionShading
 
         /// <summary>
         /// Using the flood fill algorithm, shade the region.
+        /// http://en.wikipedia.org/wiki/Flood_fill#Alternative_implementations
         /// </summary>
         /// <param name="bmp">The bitmap to color</param>
         /// <param name="start">The starting physical point (pixel). Should be inside the region.</param>
@@ -120,50 +117,51 @@ namespace DynamicGeometry.UI.RegionShading
         /// <param name="color2">The second color (ARGB) to shade with</param>
         private void FloodFill(WriteableBitmap bmp, Point start, CoordinateSystem cs, int color1, int color2)
         {
-            /* This algorithm is recursive by nature, but with large triangles it may overflow the stack.
-             * (This is experimentally pretty easy to do, so it is a real problem.)
-             * Therefore, we create our own stack that will not overflow and crash the program.
-             * Very large figures still take a noticeable amount of time to shade, but it seems acceptable.
-             * NOTE: May be able to speed up by reducing the number of isInRegion tests.
-             */
-            Stack<Point> toCheck = new Stack<Point>();
-            List<Point> checkedPoints = new List<Point>();
+            Queue<Point> toCheck = new Queue<Point>();
 
-            toCheck.Push(start);
-
-            while (toCheck.Count > 0) //Simulated Recursion
+            toCheck.Enqueue(start);
+            while (toCheck.Count > 0)
             {
-                Point pt = toCheck.Pop();
-
-                checkedPoints.Add(pt);
-
+                Point pt = toCheck.Dequeue();
                 if (Test(bmp, pt, cs))
                 {
-                    //Color the pixel and add its neighbors to the stack.
-                    bmp.Pixels[ToPixel(bmp, pt)] = Color(pt, color1, color2);
+                    //Find west-east points to color
+                    Point west = pt, east = pt;
+                    while (Test(bmp, west, cs))
+                    {
+                        west.X -= 1;
+                    }
+                    while (Test(bmp, east, cs))
+                    {
+                        east.X += 1;
+                    }
 
-                    AddPointToStack(toCheck, checkedPoints, new Point(pt.X - 1, pt.Y));
-                    AddPointToStack(toCheck, checkedPoints, new Point(pt.X + 1, pt.Y));
-                    AddPointToStack(toCheck, checkedPoints, new Point(pt.X, pt.Y - 1));
-                    AddPointToStack(toCheck, checkedPoints, new Point(pt.X, pt.Y + 1));
+                    //Color all points in between west and east...
+                    west.X += 1;
+                    while (west.X < east.X)
+                    {
+                        //Color the pixel
+                        bmp.Pixels[ToPixel(bmp, west)] = Color(west, color1, color2);
+
+                        //Check north
+                        Point north = new Point(west.X, west.Y - 1);
+                        if (Test(bmp, north, cs))
+                        {
+                            toCheck.Enqueue(north);
+                        }
+
+                        //Check south
+                        Point south = new Point(west.X, west.Y + 1);
+                        if (Test(bmp, south, cs))
+                        {
+                            toCheck.Enqueue(south);
+                        }
+
+                        //Advance east
+                        west.X += 1;
+                    }
                 }
             }
-        }
-
-        /// <summary>
-        /// Avoid additions of points to the stack that have already been verified. This is required for concave polygons and approximations.
-        /// </summary>
-        /// <param name="theStack">Stack of points to verify (with flood-filling)</param>
-        /// <param name="checkedPts">Points we have already processed.</param>
-        /// <param name="newPt">The new candidate point to add to the stack.</param>
-        /// <returns>T/F if added to the stack.</returns>
-        private bool AddPointToStack(Stack<Point> theStack, List<Point> checkedPts, Point newPt)
-        {
-            if (theStack.Contains(newPt)) return false;
-            // if (checkedPts.Contains(newPt)) return false;
-
-            theStack.Push(newPt);
-            return true;
         }
 
         /// <summary>
